0333539b4743a2628ec16cf46aac456dfbe8b963
flokoe <koehler.florian@gmx.de>
2019-02-05 16:38:26 +0100

    Add download functionality

--- build-deb.sh
+++ build-deb.sh
@@ -8,6 +8,15 @@ set -e
 #
 ######
 
+# Donwload a release
+wget "https://github.com/dani-garcia/bitwarden_rs/releases/tag/$1"
+
+# untar
+tar xzvf "$1.tar.gz"
+
+# move files
+mv "$1.tar.gz./" ..
+
 # Patch Cargo.toml with current version number and cargo-deb specific options
 patch Cargo.toml < Cargo.toml.patch
 
